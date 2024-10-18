ffdf584052e93dbf7fca6ac28c0b536451a88ba4
rondie <mr.rondie@gmail.com>
2024-10-18 10:12:41 -0400

    log path

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware to log when .use is being used and check if the file exists
+app.use("/fonts", (req, res, next) => {
+  console.log(`Middleware used for path: ${req.path}`);
+
+  // Correct the file path by removing the leading '/font' from req.path
+  const filePath = path.join(
+    __dirname,
+    "fonts",
+    req.path.replace(/^\/font/, "")
+  );
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(`File does not exist: ${filePath}`);
+    } else {
+      console.log(`File exists: ${filePath}`);
+    }
+    next();
+  });
+});
 
 // Set up CORS to allow requests only from onetrust.com
 app.use(
@@ -13,7 +34,7 @@ app.use(
 );
 
 // Serve font files from the 'fonts' directory
-app.use("/font", express.static(path.join(__dirname, "fonts")));
+app.use("/fonts", express.static(path.join(__dirname, "fonts")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
